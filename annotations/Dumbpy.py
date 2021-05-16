7249b69af2eda552de1852c62d1aa34607a91170
Andy <data.andylytics@gmail.com>
2021-05-16 13:49:46 +0100

    Create Dumbpy.py
    Created a dummy .py file with a basic test function. Exploring use of Github Desktop.

---
+++ annotations/Dumbpy.py
@@ -0,0 +1,9 @@
+# Dummy Python File
+
+
+# This is a dummy file to see what happens when I introduce a new .py file into an existing/cloned project
+
+def testfunc():
+    text = 'Hello World' 
+    print(text)
+    return text
