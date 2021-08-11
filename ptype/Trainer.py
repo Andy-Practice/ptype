d2342b29ce08e322f3b6b838746b0c05a9f2fb56
Andy <data.andylytics@gmail.com>
2021-08-11 19:10:22 +0100

    Updated Trainer object to pd.unique() instead of np.unique()
    Hope to resolve error that traces back to np.unique() when initialising Trainer object. Error was "TypeError: '<' not supported between instances of 'float' and 'str'"

--- ptype/Trainer.py
+++ ptype/Trainer.py
@@ -58,7 +58,8 @@ class Trainer:
         self.machines = machines
         self.dfs = dfs
         self.labels = labels
-        self.unique_vals = np.concatenate([np.unique(df.values) for df in dfs])
+        # self.unique_vals = np.concatenate([np.unique(df.values) for df in dfs])
+        self.unique_vals = np.concatenate([pd.unique(df.values) for df in dfs])
         self.dfs_unique_vals_counts = Trainer.get_unique_vals_counts(dfs)
         self.machines.set_unique_values(self.unique_vals)
 
