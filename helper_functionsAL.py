59897485a8985ab19ca35b9df855e01eeea36b9e
Andy <data.andylytics@gmail.com>
2021-08-11 12:34:56 +0100

    Update helper_functionsAL.py
    Added test function

--- helper_functionsAL.py
+++ helper_functionsAL.py
@@ -6,4 +6,8 @@ def quick_timestamp():
     print ("Cell Run At: ",now.strftime("%Y-%m-%d %H:%M:%S"))
     return now
 
-    
+
+def test_function():
+    for i in range(0,2):
+        print('Hi! my name is,')
+    print('Chika chika slim shady.')
