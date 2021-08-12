c87f9080a9c78df18a9cce97b19489b4596c7505
Andy <data.andylytics@gmail.com>
2021-08-12 12:56:41 +0100

    Removed one line of contains_all() test output

--- ptype/utils.py
+++ ptype/utils.py
@@ -65,7 +65,7 @@ def llhoods_with_precision(llhoods, prec=40):
 
 def contains_all(_str, _list):
     print('_str:',_str,'type',type(_str))
-    print('_list',_list,'type',type(_list))
+    # print('_list',_list,'type',type(_list))
     res = True
     for s in _str:
         if s not in _list:
