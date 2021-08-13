a860020606971f1272aaa921bdef864e395f9e68
Andy <data.andylytics@gmail.com>
2021-08-13 10:35:42 +0100

    Reintroduced author's test code from calculate_total_error()

--- ptype/Trainer.py
+++ ptype/Trainer.py
@@ -116,9 +116,9 @@ class Trainer:
         for j, (df, df_labels) in enumerate(zip(dfs, labels)):
             for i, column_name in enumerate(list(df.columns)):
                 error += self.f_col(all_probs, j, column_name, df_labels[i] - 1)
-                # print(
-                #     column_name, self.f_col(all_probs, j, column_name, df_labels[i] - 1)
-                # )
+                print(
+                    column_name, self.f_col(all_probs, j, column_name, df_labels[i] - 1)
+                )
 
         return error
 
