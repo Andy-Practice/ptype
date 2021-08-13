612b2bf6e6c571b4b3e8ba35b7d6648229c04adb
Andy <data.andylytics@gmail.com>
2021-08-13 11:55:28 +0100

    Yet more test output

--- ptype/Trainer.py
+++ ptype/Trainer.py
@@ -115,7 +115,7 @@ class Trainer:
         error = 0.0
         for j, (df, df_labels) in enumerate(zip(dfs, labels)):
             for i, column_name in enumerate(list(df.columns)):
-                print(i,column_name,df_labels[i])
+                print(i,'-',column_name,'-',df_labels[i],'-',df_labels[i]-1,'-',len(df_labels),'-',j)
                 print(
                     column_name, self.f_col(all_probs, j, column_name, df_labels[i] - 1)
                 )
