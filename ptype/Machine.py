57725ba02950d0709b99a3081f879db62190f874
Andy <data.andylytics@gmail.com>
2021-08-18 10:38:19 +0100

    Replaced Date_Generic with A,B and C options for test

--- ptype/Machine.py
+++ ptype/Machine.py
@@ -781,12 +781,30 @@ class SubTypeNonstdDate(Machine):
         self.create_T_new()
         self.copy_to_z()
 
-class Date_Generic(Machine):
+class Date_Generic_A(Machine):
     def __init__(self):
         super().__init__()
         self.STOP_P = 1e-4
-        #self.pfsm_from_fsm("[0-9\-/.: ]+") # First attempt
-        self.pfsm_from_fsm("[0-9]{1,4}[\-/.\s]?[0-9]{1,2}[\-/.\s]?[0-9]{2,4}[\-/.\s]?")
+        self.regex = "[0-9\-/.: ]+"
+        self.pfsm_from_fsm(self.regex)
+        self.create_T_new
+        self.copy_to_z()
+
+class Date_Generic_B(Machine):
+    def __init__(self):
+        super().__init__()
+        self.STOP_P = 1e-4
+        self.regex = "[0-9]{1,4}[\-/.\s]?[0-9]{1,2}[\-/.\s]?[0-9]{2,4}[\-/.\s]?"
+        self.pfsm_from_fsm(self.regex)
+        self.create_T_new
+        self.copy_to_z()
+
+class Date_Generic_C(Machine):
+    def __init__(self):
+        super().__init__()
+        self.STOP_P = 1e-4
+        self.regex = "([0-9]{1,4}[\-/.\s]?[0-9]{1,2}[\-/.\s]?[0-9]{2,4}[\-/.\s]?)?([\s])*([0-9]{1,2}[:.]?)*"
+        self.pfsm_from_fsm(self.regex)
         self.create_T_new
         self.copy_to_z()
 
